515dcbee749fa0925281f14a3466d361e7c55fdb
Nikhil Yadav <ny619619@gmail.com>
2024-11-02 13:13:26 +0530

    content modelling done

---
+++ src/models/User.ts
@@ -0,0 +1,52 @@
+import mongoose,{Schema,Document} from "mongoose";
+
+export interface Message extends Document{
+    content : string;
+    createdAt : Date
+}
+
+const messageSchema : Schema<Message> = new Schema({
+    content:{
+        type : String,
+        required : true,
+        
+    },
+    createdAt : {
+        type : Date,
+        required : true,
+        default : Date.now
+    }
+})
+
+export interface User extends Document {
+    email : string;
+    password : string;
+    username : string;
+    verifyCode : string;
+    verifyCodeExpiry : Date;
+    isAcceptingMessage : boolean;
+    message : Message[]
+}
+
+const UserSchema:Schema<User> = new Schema({
+    email:{
+        type : String,
+        required : [true,"Email is required"],
+        unique : true,
+        match : [/^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?`~]+$/,"Please use a valid email address"]
+    },
+    password:{
+        type : String,
+        required : [true,"Password is required"]
+    },
+    username : {
+        type : String,
+        required : [true, "Username is required"],
+        trim : true,
+        unique : true
+    },
+    verifyCode : {
+        type : String,
+        required: true
+    }
+})
