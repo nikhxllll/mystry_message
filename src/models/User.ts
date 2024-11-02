f990b4cf494f73e2e8b860a44555872669a52e0d
Nikhil Yadav <ny619619@gmail.com>
2024-11-02 13:22:11 +0530

    modelling done

--- src/models/User.ts
+++ src/models/User.ts
@@ -65,3 +65,7 @@ const UserSchema:Schema<User> = new Schema({
     message : [MessageSchema]
 
 })
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User",UserSchema)
+
+export default UserModel
