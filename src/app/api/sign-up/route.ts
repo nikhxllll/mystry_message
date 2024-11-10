8031f7dcab02f1bee78a2af1a0a345bca81a938e
Nikhil Yadav <ny619619@gmail.com>
2024-11-10 15:42:11 +0530

    existingUserVerifiedByUsername function

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,12 +1,24 @@
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 import dbConnect from "@/lib/dbConnect";
-import { messageSchema } from "@/schemas/messgeSchema";
+import UserModel from "@/models/User";
 
 export async function POST(request : Request){
     await dbConnect()
 
     try {
         const {username, email,password}=await request.json()
+        const existingUserVerifiedByUsername = await UserModel.findOne({
+            username,
+            isVerified:true
+        })
+        if (existingUserVerifiedByUsername) {
+            return Response.json({
+                success:false,
+                message : "Username is already taken"
+
+            },{status:400})
+            
+        }
     } catch (error) {
         console.error("Error registering user",error)
         return Response.json({
