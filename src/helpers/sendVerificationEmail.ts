3308b7a806ca9abf23b089ce4b3093c613f66897
Nikhil Yadav <ny619619@gmail.com>
2024-11-10 15:26:14 +0530

    Resend email work completed

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,26 @@
-import { Resend } from "resend";
+import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/verificatonEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+import { url } from "inspector";
+
+
+
+export async function sendVerificationEmail(
+    email:string,
+    username : string,
+    verifyCode : string
+): Promise<ApiResponse>{
+    try {
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'Mystry Message | Verification Code',
+            react : VerificationEmail({username,otp : verifyCode}
+        });
+          
+        return({success:true,message:" Verification email sent successfully"})
+    } catch (emailError) {
+        console.error("Error sending verification email",emailError)
+        return({success:false,message:"Failed to send verification email"})
+    }
+}
