ff89e4774f0c06169241b9de1575c1904f25db91
Nikhil Yadav <ny619619@gmail.com>
2024-11-14 14:39:38 +0530

    got user data for sign in

--- src/app/api/auth/[...nextauth]/option.ts
+++ src/app/api/auth/[...nextauth]/option.ts
@@ -3,6 +3,7 @@ import { NextAuthOptions } from "next-auth";
 import bcryptjs from "bcryptjs"
 import { dbconnect } from "@/lib/dbConnect";
 import UserModel from "@/models/User";
+import { error } from "console";
 
 export const authOptions : NextAuthOptions = {providers: [
     CredentialsProvider({
@@ -12,6 +13,31 @@ export const authOptions : NextAuthOptions = {providers: [
           email: { label: "Email", type: "text"},
           password: { label: "Password", type: "password" }
         },
-        async authorize(credentials)
+        async authorize(credentials:any): Promise<any>{
+            await dbconnect()
+            try {
+                const user = await UserModel.findOne({
+                    $or:[
+                        {email :credentials.identifier},
+                        {username : credentials.identifier}
+                    ]
+                })
+                if(!user){
+                    throw new Error("No user find with this email")
+                }
+                if(!user.isVerified){
+                    throw new Error("Please verify your account before login")
+                }
+                const isPasswordCorrect:boolean = await bcryptjs.compare(credentials.password,user.password)
+                if(isPasswordCorrect){
+                    return user
+                }
+                else{
+                    throw new Error("Incorrect Password")
+                }
+            } catch (err:any) {
+                throw new Error(err)
+            }
+        }
 ]
 }
