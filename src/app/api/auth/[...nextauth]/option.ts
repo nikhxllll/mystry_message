6252cdf83f1b77a23b7f412aa7e7ba58056f320e
Nikhil Yadav <ny619619@gmail.com>
2024-11-15 14:59:46 +0530

    disabled eslint for any error

--- src/app/api/auth/[...nextauth]/option.ts
+++ src/app/api/auth/[...nextauth]/option.ts
@@ -1,43 +1,49 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import { NextAuthOptions } from "next-auth";
-import bcryptjs from "bcryptjs"
+import bcrypt from "bcryptjs"
 import { dbconnect } from "@/lib/dbConnect";
 import UserModel from "@/models/User";
-import { error } from "console";
 
-export const authOptions : NextAuthOptions = {providers: [
-    CredentialsProvider({
-        id : "credentials",
-        name: "Credentials",
+export const authOptions: NextAuthOptions = {
+    providers: [
+      CredentialsProvider({
+        id: 'credentials',
+        name: 'Credentials',
         credentials: {
-          email: { label: "Email", type: "text"},
-          password: { label: "Password", type: "password" }
+          email: { label: 'Email', type: 'text' },
+          password: { label: 'Password', type: 'password' },
         },
-        async authorize(credentials:any): Promise<any>{
-            await dbconnect()
-            try {
-                const user = await UserModel.findOne({
-                    $or:[
-                        {email :credentials.identifier},
-                        {username : credentials.identifier}
-                    ]
-                })
-                if(!user){
-                    throw new Error("No user find with this email")
-                }
-                if(!user.isVerified){
-                    throw new Error("Please verify your account before login")
-                }
-                const isPasswordCorrect:boolean = await bcryptjs.compare(credentials.password,user.password)//can cause error coz added typescript
-                if(isPasswordCorrect){
-                    return user
-                }
-                else{
-                    throw new Error("Incorrect Password")
-                }
-            } catch (err:any) {
-                throw new Error(err)
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        async authorize(credentials: any): Promise<any> {
+          await dbconnect();
+          try {
+            const user = await UserModel.findOne({
+              $or: [
+                { email: credentials.identifier },
+                { username: credentials.identifier },
+              ],
+            });
+            if (!user) {
+              throw new Error('No user found with this email');
             }
-        }
-]
+            if (!user.isVerified) {
+              throw new Error('Please verify your account before logging in');
+            }
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+            if (isPasswordCorrect) {
+              return user;
+            } else {
+              throw new Error('Incorrect password');
+            }
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          } catch (err: any) {
+            throw new Error(err);
+          }
+        },
+      }),
+    ],
+
 }
