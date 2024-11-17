eb2382700caa7fe465321663ea785b0e3d2deb9a
Nikhil Yadav <ny619619@gmail.com>
2024-11-17 10:02:06 +0530

    middleware completed setup

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,25 +1,31 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 type ConnectionObject = {
-    isConnected?: number
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  // Check if we have a connection to the database or if it's currently connecting
+  if (connection.isConnected) {
+    console.log('Already connected to the database');
+    return;
+  }
+
+  try {
+    // Attempt to connect to the database
+    const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
+
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Database connection failed:', error);
+
+    // Graceful exit in case of a connection error
+    process.exit(1);
+  }
 }
 
-const connection : ConnectionObject = {}
-
-export async function dbconnect(){
-    if(connection.isConnected){
-        console.log("Already connected")
-    }
-    try {
-        const db = await mongoose.connect(process.env.MONGODB_URI || "",{})
-        
-        connection.isConnected=db.connections[0].readyState
-
-        console.log("Mongo DB connected successfully!")
-    } catch (error) {
-        console.log("Database connection failed",error)
-        process.exit(1)//exit the process gracefully
-    }
-}
-
-// export default dbconnect()// export the function
+export default dbConnect;
