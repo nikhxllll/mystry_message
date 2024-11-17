b9ba4f84e481dcdfeaea46c42e4e515e83b1f36e
Nikhil Yadav <ny619619@gmail.com>
2024-11-17 15:15:58 +0530

    verifying code route done

--- src/app/verify-code/route.ts
+++ src/app/verify-code/route.ts
@@ -28,8 +28,25 @@ export async function POST(request:Request) {
 
         if(isCodeValid && isCodeNotExpired){
             user.isVerified = true
-            
+            await user.save()
+            return Response.json({
+                success : true,
+                message : "Account verified successfully"
+            },{status : 200}) 
+
+        } else if(!isCodeNotExpired){
+            return Response.json({
+                success : false,
+                message : "Verification code has expired please sign up again to get a new code"
+            },{status : 400}) 
+
+        } else{
+            return Response.json({
+                success : false,
+                message : "verification code is not valid"
+            },{status : 400}) 
         }
+
     } catch (error) {
         console.error("Error verifying code", error)
         return Response.json({
