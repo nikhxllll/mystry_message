f5c81c2bf9e90029005249f089a81e2e1c0a3035
Nikhil Yadav <ny619619@gmail.com>
2024-11-19 11:07:56 +0530

    changes made in route ts

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,16 +1,17 @@
-import { openai } from '@ai-sdk/openai';
-import { streamText } from 'ai';
+// import { openai } from '@ai-sdk/openai';
+// import { streamText } from 'ai';
 
-// Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
+// // Allow streaming responses up to 30 seconds
+// export const maxDuration = 30;
 
-export async function POST(req: Request) {
-  const { messages } = await req.json();
+// export async function POST(req: Request) {
+//   const { messages } = await req.json();
 
-  const result = streamText({
-    model: openai('gpt-4-turbo'),
-    messages,
-  });
+//   const result = streamText({
+//     model: openai('gpt-4-turbo'),
+//     messages,
+//   });
 
-  return result.toDataStreamResponse();
-}
+//   return  (await result).toDataStreamResponse();
+
+// }
